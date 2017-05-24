879fa7e9c8d7357945538beb38baee65db956fad
gaopeng <gaopeng@qingting.fm>
2017-05-24 17:49:37 +0800

    Fix assert crash in SDL2,remove all the mounse envent handler function from SDL_cocoawindow

--- EPlayerAPP/mac/project/EPlayer/AppDelegate.h
+++ EPlayerAPP/mac/project/EPlayer/AppDelegate.h
@@ -6,10 +6,6 @@
 //  Copyright © 2017年 EC. All rights reserved.
 //
 
-/* 
- Assertion failure at -[Cocoa_WindowListener processHitTest:] (/Users/epengao/Downloads/sdl_compiling/src/SDL2-2.0.5/src/video/cocoa/SDL_cocoawindow.m:802), triggered 1 time:
- 'isDragAreaRunning == [_data->nswindow isMovableByWindowBackground]'
-*/
 #import "EPlayerAPI.h"
 #import <Cocoa/Cocoa.h>
 
