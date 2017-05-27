fd5abaf758a132b83f1aef65886038202b88591c
gaopeng <gaopeng@qingting.fm>
2017-05-28 02:31:43 +0800

    Delete all the no need static link files and clean all the project, Set player use accurate seek

--- EPlayerSDK/src/framework/MediaCtrl.cpp
+++ EPlayerSDK/src/framework/MediaCtrl.cpp
@@ -203,7 +203,7 @@ int MediaCtrl::Seek(EC_U32 nSeekPos)
     int ret = m_pSrc->Seek(nSeekPos);
     if(Source_Err_None == ret)
     {
-        bool fastSeek = true;
+        bool fastSeek = false;
         if(m_HasA)
         {
             if(m_pADec) m_pADec->Flush();
